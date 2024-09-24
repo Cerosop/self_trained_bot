df9a43a29dee6611c627bcd6a095340881398a59
Cerosop <jmhsu920816@gmail.com>
2024-09-24 18:03:29 +0800

    first

---
+++ BookingDetails.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class BookingDetails
+    {
+        public string Food { get; set; }
+
+        public string Good { get; set; }
+
+        public string Bad { get; set; }
+    }
+}
---
+++ Dialogs/BookingDialog.cs
@@ -0,0 +1,109 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Microsoft.Recognizers.Text.DataTypes.TimexExpression;
+
+namespace Microsoft.BotBuilderSamples.Dialogs
+{
+    public class BookingDialog : CancelAndHelpDialog
+    {
+        private const string FoodStepMsgText = "Where would you like to travel to?";
+        private const string GoodStepMsgText = "Where are you traveling from?";
+
+        public BookingDialog()
+            : base(nameof(BookingDialog))
+        {
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new ConfirmPrompt(nameof(ConfirmPrompt)));
+            AddDialog(new DateResolverDialog());
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
+            {
+                FoodStepAsync,
+                GoodStepAsync,
+                BadStepAsync,
+                ConfirmStepAsync,
+                FinalStepAsync,
+            }));
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private async Task<DialogTurnResult> FoodStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var bookingDetails = (BookingDetails)stepContext.Options;
+
+            if (bookingDetails.Food == null)
+            {
+                var promptMessage = MessageFactory.Text(FoodStepMsgText, FoodStepMsgText, InputHints.ExpectingInput);
+                return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
+            }
+
+            return await stepContext.NextAsync(bookingDetails.Food, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> GoodStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var bookingDetails = (BookingDetails)stepContext.Options;
+
+            bookingDetails.Food = (string)stepContext.Result;
+
+            if (bookingDetails.Good == null)
+            {
+                var promptMessage = MessageFactory.Text(GoodStepMsgText, GoodStepMsgText, InputHints.ExpectingInput);
+                return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
+            }
+
+            return await stepContext.NextAsync(bookingDetails.Good, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> BadStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var bookingDetails = (BookingDetails)stepContext.Options;
+
+            bookingDetails.Good = (string)stepContext.Result;
+
+            if (bookingDetails.Bad == null || IsAmbiguous(bookingDetails.Bad))
+            {
+                return await stepContext.BeginDialogAsync(nameof(DateResolverDialog), bookingDetails.Bad, cancellationToken);
+            }
+
+            return await stepContext.NextAsync(bookingDetails.Bad, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> ConfirmStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var bookingDetails = (BookingDetails)stepContext.Options;
+
+            bookingDetails.Bad = (string)stepContext.Result;
+
+            var messageText = $"Please confirm, I have you traveling to: {bookingDetails.Food} from: {bookingDetails.Good} on: {bookingDetails.Bad}. Is this correct?";
+            var promptMessage = MessageFactory.Text(messageText, messageText, InputHints.ExpectingInput);
+
+            return await stepContext.PromptAsync(nameof(ConfirmPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            if ((bool)stepContext.Result)
+            {
+                var bookingDetails = (BookingDetails)stepContext.Options;
+
+                return await stepContext.EndDialogAsync(bookingDetails, cancellationToken);
+            }
+
+            return await stepContext.EndDialogAsync(null, cancellationToken);
+        }
+
+        private static bool IsAmbiguous(string timex)
+        {
+            var timexProperty = new TimexProperty(timex);
+            return !timexProperty.Types.Contains(Constants.TimexTypes.Definite);
+        }
+    }
+}
